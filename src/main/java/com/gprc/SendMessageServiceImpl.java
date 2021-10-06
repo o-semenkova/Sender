245ab92e2c2435e2361e4a60e0a4580bb3509494
Oksana Semenkova <ext-oksana.semenkova@here.com>
2021-10-06 19:21:45 +0300

    Unused imports were deleted

--- src/main/java/com/gprc/SendMessageServiceImpl.java
+++ src/main/java/com/gprc/SendMessageServiceImpl.java
@@ -1,10 +1,6 @@
 package com.gprc;
 
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.springframework.stereotype.Service;
 
