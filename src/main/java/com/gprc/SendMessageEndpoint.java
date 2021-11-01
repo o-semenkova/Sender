e42cf782435b74cee81f96fc35046e01f2a86111
Oksana Semenkova <ext-oksana.semenkova@here.com>
2021-11-01 16:15:25 +0200

    2-iteration-add write_concern parameter

--- src/main/java/com/gprc/SendMessageEndpoint.java
+++ src/main/java/com/gprc/SendMessageEndpoint.java
@@ -1,8 +1,14 @@
 package com.gprc;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.grpc.LogMessage;
 
 @RestController
 public class SendMessageEndpoint {
@@ -15,5 +21,13 @@ public class SendMessageEndpoint {
   @GetMapping("/send")
   public String send() {
     return messageService.send();
+  }
+
+  @PostMapping(value = "/sendMsg",
+               consumes = MediaType.APPLICATION_JSON_VALUE,
+               produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<String> send(@RequestBody LogMessage message){
+    messageService.sendmsg(message);
+    return ResponseEntity.ok().build();
   }
 }
